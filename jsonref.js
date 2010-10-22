e8bf1948c968c82733ab0be18050f5e9ece5cd49
Hunter Loftis <hunter@skookum.com>
2010-10-22 17:11:03 -0400

    First version. Seems to work. Probably buggy, would love some real tests and improvements.

---
+++ jsonref.js
@@ -0,0 +1,115 @@
+/**
+ * JSON + Object references wrapper
+ * 
+ * @author Hunter Loftis <hunter@skookum.com>
+ * @license The MIT license.
+ * @copyright Copyright (c) 2010 Skookum, skookum.com
+ */
+ 
+;(function() {
+
+  function parse(json) {
+    var jsref = JSON.parse(json);
+    var jsref_objects = jsref.objects;
+    
+    function dereference(item) {
+      if (typeof(item) === "string") {
+        if (item.slice(0, 15) === "_jsref_objects[") {
+          var ref_index = item.slice(15, -1);
+          return jsref_objects[ref_index];
+        }
+      }
+      return item;
+    }
+    
+    var i = jsref_objects.length;
+    while (i--) {
+      var obj = jsref_objects[i];
+      for (var key in obj) {
+        if (obj.hasOwnProperty(key)) {
+          obj[key] = dereference(obj[key]);          
+        }
+      }
+    }
+    
+    return dereference(jsref.root);
+  }
+  
+  function stringify(object) {
+    var jsref_objects = [];  
+    
+    function store_objects(object) {
+      if (typeof(object) === "object") {                
+        var jsref_index = jsref_objects.indexOf(object);
+        if (jsref_objects.indexOf(object) === -1) {      // Obj hasn't been referenced before, so we need to store it
+          jsref_objects.push(object);
+        }
+        // Save all the properties of this object recursively
+        for (var key in object) {
+          if (object.hasOwnProperty(key)) {
+            store_objects(object[key]);
+          }
+        }
+      }
+    }    
+    store_objects(object);
+    
+    function reference(item) {
+      return (typeof(item) === "object") ? "_jsref_objects[" + jsref_objects.indexOf(item) + "]" : item;
+    }
+    
+    // Clone each object in jsref_objects, replacing any object-type key with a reference to that object's index
+    // store the cloned "flat" reference objects in jsref_objects_reference
+    var jsref_objects_reference = new Array(jsref_objects.length),
+        i = jsref_objects.length;
+    while(i--) {
+      var obj = jsref_objects[i];
+      var obj_clone = (obj instanceof Array) ? [] : {};    
+      for (var key in obj) {
+        if (obj.hasOwnProperty(key)) {
+          obj_clone[key] = reference(obj[key]);        
+        }
+      }
+      jsref_objects_reference[i] = obj_clone;
+    }
+  
+    return JSON.stringify({
+      root: reference(object),
+      objects: jsref_objects_reference
+    });  
+  }
+  
+  window.jsonref = {
+    stringify: stringify,
+    parse: parse
+  }
+
+})();
+
+
+var stuff = {whatever: "Goes here"};
+var other_stuff = {pointer_to: stuff};
+var test_object = {
+  objects: stuff,
+  references: other_stuff,
+  array: [0, 1, 2, other_stuff, 4],
+  func: function() {
+    console.log("What happens with a function?");
+  }
+};
+
+console.log("\noriginal object:");
+console.dir(test_object);
+
+console.log("\nJSON.stringified:");
+console.log(JSON.stringify(test_object));
+
+var str = jsref.stringify(test_object);
+
+console.log("\njsonref.stringified:");
+console.log(str);
+
+var parsed = jsonref.parse(str);
+
+console.log("\njsonref.parse re-created object:");
+console.dir(parsed);
