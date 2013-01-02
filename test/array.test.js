f0d500807d21458a7c853abbb9f762d3787d65b7
Hunter Loftis <hunter@hunterloftis.com>
2013-01-02 11:43:15 -0500

    added array test

---
+++ test/array.test.js
@@ -0,0 +1,28 @@
+var mocha = require('mocha');
+var assert = require('chai').assert;
+
+var Cryo = require('../lib/cryo');
+
+describe('Array', function() {
+
+  it('should hydrate a one-dimensional array', function() {
+    var original = [1, 2, 3, 'a', 'b', 'c'];
+    var stringified = Cryo.stringify(original);
+    var hydrated = Cryo.parse(stringified);
+
+    assert.deepEqual(hydrated, original);
+  });
+
+  it('should hydrate a multi-dimensional array', function() {
+    var original = [
+      [ 0, 1, 2 ],
+      [ 3, 4, 5 ],
+      [ 'a', 'b', 'c' ]
+    ];
+    var stringified = Cryo.stringify(original);
+    var hydrated = Cryo.parse(stringified);
+
+    assert.deepEqual(hydrated, original);
+  });
+
+});
