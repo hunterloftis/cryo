9d12ae115f09585ddd0efcab3f25b1af1ee67d2d
Hunter Loftis <hunter@hunterloftis.com>
2013-01-02 10:41:09 -0500

    renamed to cryo, bringing this back from the dead

--- jsonref.js
+++ jsonref.js
@@ -1,17 +1,17 @@
 /**
  * JSON + Object references wrapper
- * 
+ *
  * @author Hunter Loftis <hunter@skookum.com>
  * @license The MIT license.
  * @copyright Copyright (c) 2010 Skookum, skookum.com
  */
- 
+
 ;(function() {
 
   function parse(json) {
     var jsref = JSON.parse(json);
     var jsref_objects = jsref.objects;
-    
+
     function dereference(item) {
       if (typeof(item) === "string") {
         if (item.slice(0, 15) === "_jsref_objects[") {
@@ -21,25 +21,25 @@
       }
       return item;
     }
-    
+
     var i = jsref_objects.length;
     while (i--) {
       var obj = jsref_objects[i];
       for (var key in obj) {
         if (obj.hasOwnProperty(key)) {
-          obj[key] = dereference(obj[key]);          
+          obj[key] = dereference(obj[key]);
         }
       }
     }
-    
+
     return dereference(jsref.root);
   }
-  
+
   function stringify(object) {
-    var jsref_objects = [];  
-    
+    var jsref_objects = [];
+
     function store_objects(object) {
-      if (typeof(object) === "object") {                
+      if (typeof(object) === "object") {
         var jsref_index = jsref_objects.indexOf(object);
         if (jsref_objects.indexOf(object) === -1) {      // Obj hasn't been referenced before, so we need to store it
           jsref_objects.push(object);
@@ -51,38 +51,38 @@
           }
         }
       }
-    }    
+    }
     store_objects(object);
-    
+
     function reference(item) {
       return (typeof(item) === "object") ? "_jsref_objects[" + jsref_objects.indexOf(item) + "]" : item;
     }
-    
+
     // Clone each object in jsref_objects, replacing any object-type key with a reference to that object's index
     // store the cloned "flat" reference objects in jsref_objects_reference
     var jsref_objects_reference = new Array(jsref_objects.length),
         i = jsref_objects.length;
     while(i--) {
       var obj = jsref_objects[i];
-      var obj_clone = (obj instanceof Array) ? [] : {};    
+      var obj_clone = (obj instanceof Array) ? [] : {};
       for (var key in obj) {
         if (obj.hasOwnProperty(key)) {
-          obj_clone[key] = reference(obj[key]);        
+          obj_clone[key] = reference(obj[key]);
         }
       }
       jsref_objects_reference[i] = obj_clone;
     }
-  
+
     return JSON.stringify({
       root: reference(object),
       objects: jsref_objects_reference
-    });  
+    });
   }
-  
+
   jsonref = {
     stringify: stringify,
     parse: parse
-  }
+  };
 
 })();
 
